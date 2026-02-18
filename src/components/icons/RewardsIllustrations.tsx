583977a391c5d68b5a82977411720ae32bbb1baa
Luke Carnevale <lukecarnevale19@gmail.com>
2026-02-18 02:14:34 -0500

    Fix reward card wave offset

--- src/components/icons/RewardsIllustrations.tsx
+++ src/components/icons/RewardsIllustrations.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import Svg, { Path, G, Ellipse, Circle, Rect } from 'react-native-svg';
 
 export const WaveBackground: React.FC = () => (
-  <Svg style={StyleSheet.absoluteFill} viewBox="0 0 400 200" preserveAspectRatio="xMidYMid slice">
-    <Path d="M-100 120 Q-25 100 50 120 Q125 140 200 120 Q275 100 350 120 Q425 140 500 120 L500 200 L-100 200 Z" fill="white" opacity={0.05} />
-    <Path d="M-50 145 Q25 125 100 145 Q175 165 250 145 Q325 125 400 145 Q475 165 550 145 L550 200 L-50 200 Z" fill="white" opacity={0.07} />
-    <Path d="M-80 170 Q-5 155 70 170 Q145 185 220 170 Q295 155 370 170 Q445 185 520 170 L520 200 L-80 200 Z" fill="white" opacity={0.05} />
-  </Svg>
+  <View style={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: 60 }} pointerEvents="none">
+    <Svg width="100%" height="100%" viewBox="0 0 400 80" preserveAspectRatio="xMidYMax slice">
+      <Path d="M-100 20 Q-25 5 50 20 Q125 35 200 20 Q275 5 350 20 Q425 35 500 20 L500 80 L-100 80 Z" fill="white" opacity={0.05} />
+      <Path d="M-50 40 Q25 25 100 40 Q175 55 250 40 Q325 25 400 40 Q475 55 550 40 L550 80 L-50 80 Z" fill="white" opacity={0.07} />
+      <Path d="M-80 60 Q-5 50 70 60 Q145 70 220 60 Q295 50 370 60 Q445 70 520 60 L520 80 L-80 80 Z" fill="white" opacity={0.05} />
+    </Svg>
+  </View>
 );
 
 export const HeroFishIllustration: React.FC = () => (
